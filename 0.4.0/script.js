14fbf3a139d1921ed49d41c85cfe2c03ffa21ea2
HebSP <hsp2@cin.ufpe.br>
2025-11-25 17:49:39 -0300

    corigindo versionamento

--- 0.4.0/script.js
+++ 0.4.0/script.js
@@ -19,14 +19,9 @@ function ordenarCamadas(camadas) {
             if (nomeA > nomeB) return 1;
             return 0;
     }));
-    ordenadas[0].forEach((habilidade, index) => {
-        habilidade.index = index;
-    });
-
     // para as camadas seguintes usar as colunas como referencia
     // para cada camada, ordenar os elementos com base na proximidade dos pré-requisitos anteriores
     camadas.forEach((camada) => {
-
         const colunaMap = new Map();
         ordenadas.forEach((anteriores) => {
             anteriores.forEach((habilidade, colIndex) => {
@@ -43,13 +38,95 @@ function ordenarCamadas(camadas) {
             const medColB = preReqB.length > 0 ? (preReqB.map(req => colunaMap.get(req) ?? Infinity).reduce((sum, val) => sum + val, 0) / preReqB.length) : Infinity;
             return medColA - medColB;
         });
-        camadaOrdenada.forEach((habilidade, index) => {
-            habilidade.index = index;
-            habilidade.posicaoRelativa = index - (camadaOrdenada.length-1)/2;
-        });
         ordenadas.push(camadaOrdenada);
     });
     return ordenadas;
+}
+
+function ordenarintermediarios(camadas) { // estamos fazendo isso aqui agora
+    // ordenar os nodos intermediários para ficarem no centro dos seus pré-requisitos e filhos
+    console.log('Ordenando nodos intermediários...');
+
+    let avaliados = [];
+    camadas.forEach((camada, camadaIndex) => {
+        
+
+        const intermediarios = camada.filter(node => node.classList.contains('nodo-intermediario'));
+        intermediarios.forEach(intermediario => {
+            if (avaliados.includes(intermediario)) return; // já foi avaliado
+            
+            // extrair keyPai e keyFilho do id do intermediário
+            const idParts = (((intermediario.id.split('-')).slice(1)).slice(0, -2)).join('-').split('-to-');
+            const keyPai = idParts[0];
+            const keyFilho = idParts[1];
+
+            console.log("Ordenando intermediário:", intermediario.id, "\nPai:", keyPai, "\nFilho:", keyFilho);
+
+            // encontrar os índices do pai na camada anterior
+            const camadaPaiIndex = camadaIndex - 1;
+            const camadaPai = camadas[camadaPaiIndex];
+            const indexPai = camadaPai ? camadaPai.findIndex(node => node.id === keyPai) : -1;
+            
+            if (indexPai === -1) {
+                console.warn(`Pai com key ${keyPai} não encontrado na camada ${camadaPaiIndex}`);
+            }
+            
+            // encontrar o índice dos meios
+            let indexMedios = [];
+            let linhagem = [];
+            loop = true;
+            while (loop) {
+            
+                const camadaMedioIndex = camadaIndex + indexMedios.length;
+                const camadaMedio = camadas[camadaMedioIndex];
+                if (camadaMedio) {
+                    //console.log("procurando ",`intermediario-${keyPai}-to-${keyFilho}-layer-${camadaMedioIndex}`, "em \n",camadaMedio);
+                    const indexMedioAtual = camadaMedio ? camadaMedio.findIndex(node => node.id === `intermediario-${keyPai}-to-${keyFilho}-layer-${camadaMedioIndex}`) : -1;
+                    if (indexMedioAtual === -1) {
+                        loop = false;
+                    }else{
+                        //console.log(`Encontrado intermediário na camada ${camadaMedioIndex} com índice ${indexMedioAtual}`);
+                        linhagem.push(camadaMedio[indexMedioAtual]);
+                        avaliados.push(camadaMedio[indexMedioAtual]);
+                        indexMedios.push(indexMedioAtual);
+                    } 
+                } else {
+                    loop = false;
+                    console.warn(`Camada média com índice ${camadaMedioIndex} não encontrada.`);
+                }
+            }
+
+            console.log("testados até agora:", avaliados);
+
+            // encontrar o índice do filho
+            const camadaFilhoIndex = camadaIndex + indexMedios.length;
+            const camadaFilho = camadas[camadaFilhoIndex];
+
+            console.log("procurando filho", keyFilho, "em \n",camadaFilho);
+            const indexFilho = camadaFilho ? camadaFilho.findIndex(node => node.id === keyFilho) : -1;
+            
+            if (indexFilho != -1) {console.log(`Encontrado filho na camada ${camadaFilhoIndex} com índice ${indexFilho}`);}
+
+            // calcular o novo índice como a média dos índices do pai e do filho
+            let novosIndex = new Array(indexMedios.length).fill(-1);
+            console.log("Índices atuais dos intermediários:", indexMedios);
+            console.log("Cálculo de novos índices para intermediários entre pai índice", indexPai, "e filho índice", indexFilho);
+            if (indexPai !== -1 && indexFilho !== -1) {
+                novosIndex.forEach((_, i) => {
+
+                    novosIndex[i] = Math.round(indexPai/camadaPai.length + (indexFilho/camadaFilho.length - indexPai/camadaPai.length) * (i+1)/ (indexMedios.length + 1 ) * camadas[camadaIndex+i].length);
+                    
+                    // mover o intermediário para o novo índice
+                    camadas[camadaIndex+i].splice(camadas[camadaIndex+i].indexOf(linhagem[i]), 1); // remove do índice atual
+                    camadas[camadaIndex+i].splice(novosIndex[i], 0, linhagem[i]); // insere no novo índice
+
+                    console.log(`Intermediário ${linhagem[i].id} movido para o índice ${novosIndex[i]} na camada ${camadaIndex + i}`);
+                });
+            } else console.warn(`Não foi possível calcular novo índice para intermediário ${intermediario.id} e toda a sua linha devido a índices inválidos de pai ou filho.`);
+            
+        });
+    });
+    return camadas;
 }
 
 function criarCamada(filhos = []) {
@@ -64,6 +141,7 @@ function criarCamada(filhos = []) {
 }
 
 // Função para criar um elemento de habilidade
+
 function criarHabilidade(key,nome, imagemUrl, descricao) {
     const node = document.createElement('div');
     node.classList.add('skill');
@@ -96,7 +174,7 @@ function criarHabilidade(key,nome, imagemUrl, descricao) {
     return node;
 }
 // cria setas entre as habilidades usando svg (sera substituido)
-function criarSetas(ligações, container) { 
+function criarSetas(ligações, container, camadas) { 
     const svgNS = "http://www.w3.org/2000/svg";
     const svg = document.createElementNS(svgNS, "svg");
     svg.setAttribute("width", "100%");
@@ -139,24 +217,52 @@ function criarSetas(ligações, container) {
         
         const path = document.createElementNS(svgNS, "path");
 
-        path.setAttribute("d", `M ${inicioX} ${inicioY} C ${inicioX} ${(inicioY + fimY) / 2} , ${fimX} ${(inicioY + fimY) / 2} , ${fimX} ${fimY}`);
+        // Descobrir em que camada está cada node
+        const getCamadaIndex = (node) => {
+            for (let i = 0; i < camadas.length; i++) {
+            if (camadas[i].includes(node)) return i;
+            }
+            return -1;
+        };
+        const camadaInicio = getCamadaIndex(ligação[0]);
+        const camadaFim = getCamadaIndex(ligação[1]);
+
+        if (camadaFim === camadaInicio + 1) { // se a ligação é entre camadas adjacentes
+            
+            path.setAttribute("d", `M ${inicioX} ${inicioY} C ${inicioX} ${(inicioY + fimY) / 2} , ${fimX} ${(inicioY + fimY) / 2} , ${fimX} ${fimY}`);
+        } else {
+            path.setAttribute("d", `M ${inicioX} ${inicioY} C ${inicioX} ${(inicioY + fimY) / 2} , ${fimX} ${(inicioY + fimY) / 2} , ${fimX} ${fimY}`);
+            // se a ligação é entre camadas não adjacentes, desenhar linha com curva, deviando dos objetos no meio do caminho
+            // const path0 = document.createElementNS(svgNS, "path");
+            // path0.setAttribute("d", `M ${inicioX} ${inicioY} C ${inicioX} ${(inicioY + 28)} , ${fimX} ${(inicioY + 28)} , ${fimX} ${inicioY + 56}`);
+            // path0.setAttribute("stroke", "black");
+            // path0.setAttribute("stroke-width", "5");
+            // path0.setAttribute("fill", "none");
+            // path0.setAttribute("stroke-dasharray", "5,5"); // linha tracejada para indicar salto de camada
+            // path0.id = `path-${ligação[0].id}-to-${ligação[1].id}-start`;
+            // svg.appendChild(path0);
+            // path.setAttribute("d", `M ${fimX} ${inicioY + 56} C ${fimX} ${(inicioY + fimY) / 2} , ${fimX} ${(inicioY + fimY) / 2} , ${fimX} ${fimY}`);
+            // depois fazer algo mais detalhado
+        }
         path.setAttribute("stroke", "black");
         path.setAttribute("stroke-width", "5");
         path.setAttribute("fill", "none");
         path.setAttribute("marker-end", "url(#arrowhead)");
         path.id = `path-${ligação[0].id}-to-${ligação[1].id}`;
         svg.appendChild(path);
+    
     })
+
     return svg;
 }
 
-function criarDivSetas(ligações, container) {
-    const setasContainer = document.createElement('div');
-    setasContainer.classList.add('setas-container');
+function criarDivSetas(ligações, container, camadas) {
+    const setasContainer = document.createElement('div')
+    setasContainer.classList.add('setas-container')
     
-    setasContainer.appendChild(criarSetas(ligações,container));
+    setasContainer.appendChild(criarSetas(ligações,container, camadas))
 
-    return setasContainer;
+    return setasContainer
 }
 
 function criarCamadas(habilidades) {
@@ -202,7 +308,9 @@ function criarCamadas(habilidades) {
 }
 
 function getCamadaIndex(node, camadas) {
-    for (let i = 0; i < camadas.length; i++) if (camadas[i].includes(node)) return i;
+    for (let i = 0; i < camadas.length; i++) {
+        if (camadas[i].includes(node)) return i;
+    }
     return -1;
 }
 
@@ -210,48 +318,26 @@ function criarLigações(habilidades, camadas) {
 
     let ligações = [];
     habilidades.forEach(habilidade => {
-        const camadaHabilidade = getCamadaIndex(habilidade.node, camadas);
+        let camadaHabilidade = getCamadaIndex(habilidade.node, camadas);
         habilidade.pre_requisitos.forEach(req => {
             const pai = habilidades.find(h => h.key === req);
             if (pai) {
-                const camadaPai = getCamadaIndex(pai.node, camadas);
+                let camadaPai = getCamadaIndex(pai.node, camadas);
                 if (camadaPai + 1 === camadaHabilidade) {
                     ligações.push([pai.node, habilidade.node]);
                 } else{
                     // ligação entre camadas não adjacentes
-                    let tempPai = pai, i = 1;
-                    let meios = [];
-                    while (camadaPai + i < camadaHabilidade) {
+                    let tempPai = pai;
+                    while (camadaPai + 1 < camadaHabilidade) {
                         // criar nó intermediário fictício
                         const nodoIntermediário = document.createElement('div');    
                         nodoIntermediário.classList.add('nodo-intermediario');
-                        nodoIntermediário.id = `intermediario-${tempPai.key}-to-${habilidade.key}-layer-${camadaPai + i}`;
-                        //camadas[camadaPai + i].push(nodoIntermediário);
-                        meios.push({node:nodoIntermediário,camada:camadaPai + i});
+                        nodoIntermediário.id = `intermediario-${tempPai.key}-to-${habilidade.key}-layer-${camadaPai + 1}`;
+                        camadas[camadaPai + 1].push(nodoIntermediário);
                         ligações.push([pai.node, nodoIntermediário]);
                         tempPai.node = nodoIntermediário; // atualizar o pai para o próximo nó intermediário
-                        i++;
+                        camadaPai++;
                     }
-                    // encontrar a posição correta para inserir cada nó intermediário
-                    let maiorCamada = -1;
-                    camadas.forEach((camada) => {
-                        if (camada.length > maiorCamada) maiorCamada = camada.length;
-                    });
-                    let index = -1;
-                    while (meios.length > 0) {
-                        const meio = meios.shift();
-                        index = -1;
-                        
-                        const posicaoRelativa = (pai.posicaoRelativa + (pai.posicaoRelativa - habilidade.posicaoRelativa)* ((meio.camada - camadaPai)/(camadaHabilidade - camadaPai))) / 2;
-                        index = Math.round(posicaoRelativa + (camadas[meio.camada].length-1)/2);
-                        meio.node.index = index+0,5; // para evitar conflitos de índice
-                        meio.node.posicaoRelativa = posicaoRelativa;
-                        // inserir o nó intermediário na camada correta, mantendo a ordem
-                        camadas[meio.camada].splice(index, 0, meio.node);
-                        console.log(`Inserido nó intermediário ${meio.node.id} na camada ${meio.camada} na posição ${index}`);
-                    }
-
-
                     ligações.push([tempPai.node, habilidade.node]);
                 }
             }
@@ -272,7 +358,8 @@ async function carregarHabilidadesJSON() {
 
 async function gerarArvoreDeHabilidades() {
     const container = document.getElementById('arvore-container');
-
+    
+    
     // Habilidades 
     habilidades = await carregarHabilidadesJSON(); // Carrega do JSON
     
@@ -284,6 +371,8 @@ async function gerarArvoreDeHabilidades() {
     camadas = criarCamadas(habilidades); // Cada sub-array representa uma camada na árvore
     
     ligações = criarLigações(habilidades, camadas); // Criar ligações entre os nodes
+    
+    ordenarintermediarios(camadas); // Ordenar nodos intermediários
     
     // Criar e adicionar camadas ao container
     camadas.forEach(camada => {
@@ -299,7 +388,7 @@ async function gerarArvoreDeHabilidades() {
     container.appendChild(nodoPrincipal);
 
     // Adicionar setas entre as camadas
-    const containerSetas = criarDivSetas(ligações, container);
+    const containerSetas = criarDivSetas(ligações, container, camadas);
     container.appendChild(containerSetas);
 
 }
